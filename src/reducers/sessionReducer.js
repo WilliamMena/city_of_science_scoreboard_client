f0c510718bf528b9f3631c418b64e3ee939bf42e
WilliamMena <williammena1991@gmail.com>
2018-02-13 00:34:32 -0500

    Player Form updates session players correctly in each session

--- src/reducers/sessionReducer.js
+++ src/reducers/sessionReducer.js
@@ -13,7 +13,12 @@ export default function sessionsReducer(state = initialState, action) {
       var currentSession = state.find(session => session.id == action.player.session.id)
       var updatedSession = Object.assign({}, currentSession, {players: currentSession.players.concat(action.player)})
       var index = state.findIndex(session => session == currentSession)
-      return Object.assign([], state.slice(0, index), state.slice(index,-1)).concat(updatedSession)
+
+      var newState = state
+      newState[index] = updatedSession
+
+      // return Object.assign([], state.slice(0, index), state.slice(index,-1)).concat(updatedSession)
+      return Object.assign([], newState)
 
     default:
       return state;
